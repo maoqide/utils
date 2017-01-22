b12101ca61070dfb8f73b471e52d45c5cc5be1a3
qmao <qmao@linkernetworks.com>
2017-01-22 17:03:37 +0800

    modified

--- tagImages.sh
+++ tagImages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 originRegistry=$1
-TagRegistry=$2
-if  [ ! -n "$originRegistry" ] ;then
-	echo "usage: sh tagImages.sh originRegistry TagRegistry"
+tagRegistry=$2
+if  [[ ! -n "$originRegistry" || ! -n "$tagRegistry" ]] ;then
+	echo "usage: sh tagImages.sh originRegistry tagRegistry"
 	exit 0
 fi
 
 images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep $originRegistry | sed 's#/# #g' | awk '{print $2}')
 for image in $images
 do
-echo executed: docker tag $originRegistry/$image $TagRegistry/$image
-docker tag $originRegistry/$image $TagRegistry/$image
+echo executed: docker tag $originRegistry/$image $tagRegistry/$image
+docker tag $originRegistry/$image $tagRegistry/$image
 done
