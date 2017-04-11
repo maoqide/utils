4292913910d950f5e3babb99712a558b4806ab6f
maoqide <maoqiyx@163.com>
2017-04-11 15:37:54 +0800

    modify tagImages.sh

--- tagImages.sh
+++ tagImages.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 originRegistry=$1
 tagRegistry=$2
+keyword=$3
 if  [[ ! -n "$originRegistry" || ! -n "$tagRegistry" ]] ;then
-	echo "usage: sh tagImages.sh originRegistry tagRegistry"
+	echo "usage: sh tagImages.sh originRegistry tagRegistry [keyword]"
 	exit 0
 fi
 
-images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep $originRegistry | sed 's#/# #g' | awk '{print $2}')
+images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep $originRegistry | grep $keyword | sed 's#/# #g' | awk '{print $2}')
 for image in $images
 do
 echo executed: docker tag $originRegistry/$image $tagRegistry/$image
