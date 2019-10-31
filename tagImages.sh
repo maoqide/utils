76884df33cc202e4334df2dfa29d6559f4ca7e53
maoqide <maoqiyx@163.com>
2019-10-31 14:13:45 +0800

    update tagimage

--- tagImages.sh
+++ tagImages.sh
@@ -6,8 +6,10 @@ if  [[ ! -n "$originRegistry" || ! -n "$tagRegistry" ]] ;then
 	echo "usage: sh tagImages.sh originRegistry tagRegistry [keyword]"
 	exit 0
 fi
-
-images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep $originRegistry | grep $keyword | sed 's#/# #g' | awk '{print $2}')
+if  [[ ! -n "$keyword" ]] ;then
+	keyword=$originRegistry
+fi
+images=$(docker images --format '{{.Repository}}:{{.Tag}}' | grep $originRegistry | grep $keyword | sed 's#/# #g' | awk '{print $NF}')
 for image in $images
 do
 echo executed: docker tag $originRegistry/$image $tagRegistry/$image
